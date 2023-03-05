dc327b7cd1ac06bcb9ffdcf3a76ae1fada8b4a34
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-03-05 13:17:54 +0530

    chats fetch backend

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -1,33 +1,57 @@
+const sequelize = require('sequelize');
 const dataChecks = require('../Models/functions')
 const chatModel = require('../Models/chatsModel')
+const userloginModel = require('../Models/loginsmodel')
 
 
 exports.postchat = async (req, res) => {
 
 
 
-    try{
-        console.log("Our request body is..",req.body.chat)
+    try {
+        console.log("Our request body is..", req.body.chat)
         const token = req.header("Authorization");
         console.log(token);
-    const userid = await dataChecks.tokenAuthentication(token)
-    console.log(userid)
-    
-    
-    await chatModel.create(({
-        chat : req.body.chat,
-        userLoginId : userid
-    })).then(respose=>{
-        res.status(200).json({success: true})
+        const userid = await dataChecks.tokenAuthentication(token)
+        console.log(userid)
 
-   
-    })
-    }catch(err){
-        console.log(err," at postChat Controller")
+
+        await chatModel.create(({
+            chat: req.body.chat,
+            userLoginId: userid
+        })).then(respose => {
+            res.status(200).json({ success: true })
+
+
+        })
+    } catch (err) {
+        console.log(err, " at postChat Controller")
     }
-   
+
 
 
 
 }
 
+exports.getchat = async (req, res, next) => {
+    const token = req.header("Authorization");
+    console.log(token);
+    const userid = await dataChecks.tokenAuthentication(token)
+    console.log(userid)
+
+
+    const chatstore = await chatModel.findAll({
+        include: userloginModel,
+        attribute : ['name']
+        
+
+    }).then(response => { return response })
+
+    // const stringi = JSON.stringify(chatstore);
+    // const par = JSON.parse(stringi)
+
+    if (chatstore != null){
+       res.status(200).json({chat : chatstore})
+    }
+
+}
--- Routes/chatRoutes.js
+++ Routes/chatRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const chatController = require('../Controllers/chatController')
 
 router.post('/postchat', chatController.postchat)
+router.get('/getchat', chatController.getchat)
 
 
 module.exports = router
