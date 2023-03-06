e8e5033cdf495ae8a234fe8d114535d04d28f784
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-03-06 08:26:18 +0530

    optimising chat - backend

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -2,6 +2,7 @@ const sequelize = require('sequelize');
 const dataChecks = require('../Models/functions')
 const chatModel = require('../Models/chatsModel')
 const userloginModel = require('../Models/loginsmodel')
+const { QueryTypes } = require('sequelize');
 
 
 exports.postchat = async (req, res) => {
@@ -40,15 +41,31 @@ exports.getchat = async (req, res, next) => {
     console.log(userid)
 
 
+    // const chatstore = await userloginModel.findAll({
+    //     attributes: ['name',[sequelize.col('chatsTables.chat'),'chat']],
+    //     include: [{ 
+    //         model :chatModel 
+    //     }]
+    // }).then(response => { return response })
     const chatstore = await chatModel.findAll({
-        include: userloginModel,
-        attribute : ['name']
-        
-
+        attributes: ['id','chat'],
+        include: [{ 
+            model : userloginModel,
+            attributes : ['name'],
+            required : true
+        }],
+      order : ['id']
     }).then(response => { return response })
 
-    // const stringi = JSON.stringify(chatstore);
-    // const par = JSON.parse(stringi)
+    // const chatstore = await chatModel.findAll({
+    //     include: userloginModel,
+    //     attribute : ['name']
+
+
+    const stringi = JSON.stringify(chatstore);
+    const par = JSON.parse(stringi)
+    console.log(par)
+    // console.log(chatstore)
 
     if (chatstore != null){
        res.status(200).json({chat : chatstore})
