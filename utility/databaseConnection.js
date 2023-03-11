a10d2ca393960692d7cbbb9ae827fa0cd9dfc3fa
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-03-11 11:27:22 +0530

    connection file chages

--- utility/databaseConnection.js
+++ utility/databaseConnection.js
@@ -1,8 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize ('chatapp','root','H3lloworld!',{
+const sequelize = new Sequelize (process.env.DB_NAME,process.env.MYSQL_USER,process.env.MYSQL_PASS,{
     dialect : 'mysql',
-    host : 'localhost'
+    host : process.env.DB_HOST
 })
 
 module.exports = sequelize;
+
+
+// MYSQL_USER=admin
+// MYSQL_PASS=H3lloworld!
+// DB_NAME=chatapp
+// DB_HOST=chatappdb.ccjbhdxwrrm6.us-east-1.rds.amazonaws.com
